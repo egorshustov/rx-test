385ba96975b4a62b746cde0d266a6ce252b23aa8
Egor Shustov <egorshustov.93@gmail.com>
2019-06-07 00:21:34 +0500

    Disposables

--- app/src/main/java/com/egorshustov/rxtest/MainActivity.kt
+++ app/src/main/java/com/egorshustov/rxtest/MainActivity.kt
@@ -8,19 +8,24 @@ import io.reactivex.FlowableSubscriber
 import io.reactivex.Observable
 import io.reactivex.Observer
 import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.disposables.Disposable
+import io.reactivex.functions.Consumer
 import io.reactivex.functions.Predicate
 import io.reactivex.schedulers.Schedulers
 import org.reactivestreams.Subscription
 
 class MainActivity : AppCompatActivity() {
 
+    val disposables = CompositeDisposable()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         //observableExample()
-        flowable()
+        //flowable()
+        disposable()
     }
 
     private fun observableExample() {
@@ -77,6 +82,51 @@ class MainActivity : AppCompatActivity() {
                 override fun onComplete() {
                 }
             })
+    }
+
+    private fun disposable() {
+        val taskObservable: Observable<Task> = Observable
+            .fromIterable(DataSource.createTasksList())
+            .subscribeOn(Schedulers.io())
+            .filter(object : Predicate<Task> {
+                override fun test(t: Task): Boolean {
+                    Log.d(TAG, "test: ${Thread.currentThread().name}")
+                    Thread.sleep(1000)
+                    return t.isComplete
+                }
+            })
+            .observeOn(AndroidSchedulers.mainThread())
+
+        taskObservable.subscribe(object : Observer<Task> {
+            override fun onSubscribe(d: Disposable) {
+                Log.d(TAG, "onSubscribe: called.")
+                disposables.add(d)
+            }
+
+            override fun onNext(t: Task) {
+                Log.d(TAG, "onNext: ${Thread.currentThread().name}")
+                Log.d(TAG, "onNext: ${t.description}")
+            }
+
+            override fun onError(e: Throwable) {
+                Log.d(TAG, "onError: $e")
+            }
+
+            override fun onComplete() {
+                Log.d(TAG, "onComplete: called.")
+            }
+        })
+
+        disposables.add(taskObservable.subscribe(object : Consumer<Task> {
+            override fun accept(t: Task?) {
+            }
+        }))
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        // If using MVVM, clear disposables in ViewModel onCleared method
+        disposables.clear()
     }
 
     companion object {
